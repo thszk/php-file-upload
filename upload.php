b8f1df86eb083437c0e6aca3bae53a64fafa5d48
thszk <thsuzuqui@gmail.com>
2025-04-23 09:15:11 -0400

    adicionando validação de requisição e tipo de arquivo, tratamento para nome unico e criação dinâmica do diretorio destino

--- upload.php
+++ upload.php
@@ -1,10 +1,44 @@
 <?php
 
-$diretorioDestino = 'uploads/';
+// Validação caso a request não seja POST
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    return header('Location: index.php');
+}
+
+// Validação caso a request não tenha atributo imagem como file
+if (!isset($_FILES['imagem'])) {
+    return header('Location: index.php');
+}
+
 $arquivoImagem = $_FILES['imagem'];
 
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types/Common_types
+$tiposPermitidos = ['image/jpeg', 'image/png'];
+$extensoesPermitidas = ['jpg', 'jpeg', 'png'];
+
+// Validação do tipo do arquivo
+if (!in_array($arquivoImagem['type'], $tiposPermitidos)) {
+    die('Tipo de arquivo inválido. Apenas JPG, JPEG, PNG são permitidos.');
+}
+
+// Validação da extensão do arquivo
+$extensaoArquivo = strtolower(pathinfo($arquivoImagem['name'], PATHINFO_EXTENSION));
+if (!in_array($extensaoArquivo, $extensoesPermitidas)) {
+    die('Extensão de arquivo inválida. Apenas JPG, JPEG, PNG são permitidos.');
+}
+
+$diretorioDestino = './uploads/';
+
+// Validação caso o diretório destino não exista, então cria
+if (!is_dir($diretorioDestino)) {
+    mkdir($diretorioDestino, 0777, true);
+}
+
 $caminhoTemporario = $arquivoImagem['tmp_name'];
-$caminhoDestino = $diretorioDestino . $arquivoImagem['name'];
+
+// Tratamento para salvar os arquivos com nome único
+$nomeUnico = uniqid() . '_' . $arquivoImagem['name'];
+$caminhoDestino = $diretorioDestino . $nomeUnico;
 
 $salvou = move_uploaded_file($caminhoTemporario, $caminhoDestino);
 
